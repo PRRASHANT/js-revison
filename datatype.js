28bbbff73fe726f26b35ff214d0133c3829473b9
PRRASHANT <165308821+PRRASHANT@users.noreply.github.com>
2026-05-20 11:28:05 +0530

    first commit

---
+++ datatype.js
@@ -0,0 +1,52 @@
+// basics of printing 
+console.log("Hello world ");
+
+
+process.stdout.write("Another way of printing but no newline");
+
+process.stdout.write("..........you see no new line");
+
+console.log("Printing for spoace ")
+
+console.table({ city : "Ranchi"});
+
+
+/* Datatypes
+ 1. NULL
+ 2. STRING
+ 3. NUMBER
+ 4. BOOLEAN
+ 5. Bigint(long)
+ 6. undefined 
+ 7. symbol
+ 8. object
+
+
+
+*/
+
+let age = 18;
+console.log(typeof(age));
+
+let height = null  ; 
+console.log(height); 
+
+let isloggedin = false;
+console.log(isloggedin);
+
+//object
+let usertypes = ["male" , "Female" , "others "]
+let user = { firstName : "prashant" , lastname : "Kumar"}
+
+console.log(usertypes);
+console.log(user);
+
+let getscore = age ;
+console.log(getscore);
+
+
+
+
+
+
+
