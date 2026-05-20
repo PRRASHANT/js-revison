281ab11c221a9445a7225585b26f71bbf502bbb4
PRRASHANT <165308821+PRRASHANT@users.noreply.github.com>
2026-05-20 12:55:20 +0530

    objects done

---
+++ nonprimitive.js
@@ -0,0 +1,27 @@
+//  object
+
+const username = {
+    firstname : "prashant",
+    lastName : "Kumar"
+}
+
+console.log(username);
+username.firstname = "badal"
+console.log(username);
+
+username.middlename = "kr"
+console.log(username);
+
+
+// Array 
+
+let user  = [ "prashant ", "Badal", "volleyball"]
+
+console.log(user[0])
+console.log(user[1])
+console.log(user[2])
+
+
+
+
+
