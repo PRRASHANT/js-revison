e701a2876f57ae8a73a9a4406bcc92df407aac7e
PRRASHANT <165308821+PRRASHANT@users.noreply.github.com>
2026-05-26 11:34:09 +0530

    string methods Revision

---
+++ str.js
@@ -0,0 +1,17 @@
+let s = "My name is Prashant "
+console.log(s.toUpperCase())
+console.log(s.toLowerCase())
+
+console.log(s.indexOf("a"))
+
+console.log(s.trim())
+console.log(s.toUpperCase().trim())
+
+console.log(s.slice(6,10))
+
+console.log(s.toUpperCase())
+
+console.log(s.replace("na" , "aa"))
+
+console.log(s.repeat(4))
+
